0832cdb1a8432564ff66d72110da74b326260e66
kjm-datasci <kyle.mendenhall@gmail.com>
2017-02-25 17:15:26 -0600

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+library(dplyr)
+library(xlsx)
+# library(XML)
+# library(data.table)
+setwd("/Users/kyle_mendenhall/Documents/coursera/R Data Science Program/Class 3 - Cleaning Data/Final Assignment/")
+
+# Import Data
+  train = read.table(file = "UCI HAR Dataset/train/X_train.txt")     # Train dataset
+  trainID = read.table(file = "UCI HAR Dataset/train/y_train.txt")   # Activity ID of each obs in train dataset
+  trainsubj = read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
+
+  test = read.table(file = "UCI HAR Dataset/test/X_test.txt")        # Test dataset
+  testID = read.table(file = "UCI HAR Dataset/test/y_test.txt")      # Activity ID of each obs in test dataset
+  testsubj = read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE)
+
+  features = read.table(file = "UCI HAR Dataset/features.txt")       # Col names for each var in train/test dataset
+  
+  activityLabels<-read.table("UCI HAR Dataset/activity_labels.txt", header=FALSE)  # Map activity ID to activity
+
+# Attach the activity IDs and subjects to the train and test datasets
+  train = cbind(trainsubj,trainID,train)
+  test = cbind(testsubj,testID,test)
+
+# Direction Objective #4: Create descriptive column names ----
+  fNames = as.character(features[,"V2"])          # Subset the features data to obtain the feature names
+  
+  names(train) = c("subject","activityID",fNames)
+  names(test) = c("subject","activityID",fNames)
+
+# Direction Objective #1: Merge the train and test datasets ----
+  data = rbind(train,test)
+  nrow(data) == (nrow(train)+nrow(test))
+
+# Direction Objective #2: Select only columns that contain either mean or std in their name ----
+  meanstddata = data[,c(1,2,grep("[Mm][Ee][Aa][Nn]|[Ss][Tt][Dd]",names(data)))]
+
+# Direction Objective #3: Map the Activity Names to the Activity IDs ----
+  names(activityLabels) = c("activityID","activity")
+  meanstddata = full_join(activityLabels,meanstddata,by="activityID")
+  meanstddata = select(meanstddata,-activityID)
+
+# Direction Objective #5: Create independent tidy data set with average of each variable from recent dataset ----
+  tidydata = meanstddata %>% group_by(activity,subject) %>% summarise_each(funs(mean))
